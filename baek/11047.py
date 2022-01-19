13096e34c54265b75d431d7c2a43b3bba4536122
seungjohan <cmostrillion@gmail.com>
2022-01-19 16:11:44 +0900

    11047 advanced

--- baek/11047.py
+++ baek/11047.py
@@ -7,10 +7,22 @@ count = 0
 for i in range(0, N):
     coin.append(int(sys.stdin.readline().rstrip()))
 
-while K > 0:
-    target = coin.pop()
-    count += K // target
-    K %= target
+# while & Need to check if it is Empty
+
+# while K > 0:
+#     target = coin.pop()
+#     count += K // target
+#     K %= target
+
+
+# if - for
+for i in range(N-1, 0, -1):
+    count += K // coin[i]
+    K %= coin[i]
+
+
 
 print(count)
+
+
 
