39ce9bfe873a44cc2ffac0442a50fc0dae24cf61
seungjohan <cmostrillion@gmail.com>
2022-01-19 16:03:24 +0900

    baek fix

--- baek/11047.py
+++ baek/11047.py
@@ -7,9 +7,10 @@ count = 0
 for i in range(0, N):
     coin.append(int(sys.stdin.readline().rstrip()))
 
-if K > 0:
+while K > 0:
     target = coin.pop()
     count += K // target
     K %= target
 
 print(count)
+
