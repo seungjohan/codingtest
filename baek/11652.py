a9af88fabf611c34ae9d1df83e7658f018698653
seungjohan <cmostrillion@gmail.com>
2022-01-20 15:40:09 +0900

    baekjoon 11652

--- baek/11652.py
+++ baek/11652.py
@@ -1,7 +1,19 @@
 import sys
 
 N = int(input())
-list = []
+card = {}
 
-for i in range(N):
-    num = int(sys.stdin.readline().rstip())
+for i in range(0, N):
+    num = int(sys.stdin.readline().rstrip())
+    if num in card:
+        card[num] += 1
+    else:
+        card[num] = 1
+
+# for i in range(N-1, 1, -1):
+#     if card[i-1] >= card[i]:
+#         target = card[i-1]
+
+# print(target)
+
+print(sorted(card.items(), key= lambda x: (x[1], -x[0]), reverse=True)[0][0])
