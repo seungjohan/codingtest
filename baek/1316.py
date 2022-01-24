9d4be20b2d4dbde4ee8e956931fd67b427da1193
seungjohan <cmostrillion@gmail.com>
2022-01-24 17:21:59 +0900

    baek 1316:

--- baek/1316.py
+++ baek/1316.py
@@ -1,10 +1,19 @@
-import string
 import sys
 
 N = int(input())
+result = N
 words = []
 
 for i in range(0, N):
-    words.append(string(sys.stdin.readline().rstrip()))
+    words.append(str(sys.stdin.readline().rstrip()))
+    # word = input()
 
-    
+    for j in range(0, len(words[i])-1):
+        if words[i][j] != words[i][j+1]:
+            pass
+        elif words[i][j] == words[i][j+1:]:
+            result -= 1
+            break
+
+print(words)
+print(N)
