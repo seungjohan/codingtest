cb00d33abe3dba8533375454489ffaf5181b4eda
seungjohan <cmostrillion@gmail.com>
2022-02-14 15:26:48 +0900

    [beak] 11047 another way of solve

--- baek/11047.py
+++ baek/11047.py
@@ -25,4 +25,20 @@ for i in range(N-1, 0, -1):
 print(count)
 
 
+# another way
+
+n,k = map(int, input().split())
+coin_list = list()
+count = 0
+
+for i in range(n):
+    coin_list.append(int(input()))
+
+for i in reversed(range(n)):
+    count += k // coin_list[i]
+    k %= coin_list[i]
+    
+print(count)
+
+
 
