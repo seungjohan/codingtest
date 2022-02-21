36139eaf78ef66ebb7fa559a0d9bdfd1399abf39
seungjohan <cmostrillion@gmail.com>
2022-02-21 14:58:40 +0900

    [nadongbin] moving matrix square

--- nadongbin/ewsn.py
+++ nadongbin/ewsn.py
@@ -1,7 +1,21 @@
 n = int(input())
-data = list(map(str, input().split()))
+roads = list(map(str, input().split()))
+x, y = 1, 1
 
-move = ['R', 'D', 'L', 'U']
+steps = ['R', 'D', 'L', 'U']
 dx = [1, 0, -1, 0]
-dy = [0, -1, 0, 1]
+dy = [0, 1, 0, -1]
+
+for road in roads:
+    for i in range(len(steps)):
+        if road == steps[i]:
+            nx = x+dx[i]
+            ny = y+dy[i]
+
+    if nx < 1 or ny < 1 or nx > n or ny > n:
+        continue
+    x, y = nx, ny
+
+print(x, y)
+
 
